a3f50625bad02427813c53985b2dbbf5f92cdd9e
nimeshdilshaan <nimeshdilshan126@gmail.com>
2025-08-08 18:23:21 +0530

    done home page - nimesh

--- src/pages/new.jsx
+++ src/pages/new.jsx
@@ -4,18 +4,19 @@ import React from "react";
 export default function TestPage() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello, React! 👋</h1>
-      <p>This is your test JSX page.</p>
+      <h1>Hello, kavee! 👋</h1>
+      <p>This is front end home JSX page.</p>
       <button
-        onClick={() => alert("Button Clicked!")}
+        onClick={() => alert("Moda kavee")}
         style={{
           padding: "10px 20px",
-          background: "#007bff",
+          background: "#ff8c00ff",
           color: "white",
           border: "none",
           borderRadius: "5px",
           cursor: "pointer",
         }}
+        
       >
         Click Me
       </button>
